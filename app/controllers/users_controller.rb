76a7d13dfd754c208231637528caf79a638ade8f
tkhr <karasawa@basicinc.jp>
2016-05-17 06:33:31 +0000

    fix form style and changable password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   include SessionsHelper
+  include BCrypt
   before_action :logged_in_user, only: [:edit, :update]
   before_action :set_user, only: [:edit, :update]  # :editと:updateの前だけ:set_messageを実行する
 
@@ -29,12 +30,30 @@ class UsersController < ApplicationController
   end
 
   def update
-    if @user.update(user_params)
+    update_params = user_params
+
+    # パスワード変更処理
+    if params[:user][:new_password] != "" && params[:user][:new_password]   # PWの変更要求
+      if params[:user][:new_password] == params[:user][:new_password_confirmation]   # PWの整合性チェック
+       @user = current_user
+        if @user.authenticate(user_params[:password]) # 認証
+          update_params[:password] = params[:user][:new_password]   # PW入れ替え
+        else
+          flash[:danger] = "パスワードと確認パスワードが違います"
+          render 'edit' and return
+        end
+      else
+        flash[:danger] = "パスワードが違います"
+        render 'edit' and return
+      end
+    end
+    
+    if @user.update(update_params)
       flash[:success] = "プロフィールを更新しました"
-      redirect_to user_path
+      redirect_to user_path and return
     else
       @user = current_user
-      render 'edit'
+      render 'edit' and return
     end
   end
 
